11b77f36f8aeb13d8931d8c5848b5ee6dcd46862
AdsonCoelho <adson.coelho@gmail.com>
2022-08-10 19:03:39 -0400

    Organizando o formulário de login e css

---
+++ login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Blog da torcida</title>
+        <meta charset="utf-8">
+        <link a href="style.css" type="text/css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="cache-control" content="no-cache" />		
+        
+    </head>
+    <body>
+        <nav class="menu">
+            <a class="" href="index.html">Inicio</a>
+            <a class="" href="sobre.html">Sobre</a>
+            <a class="" href="login.html">Login</a>			
+        </nav>
+
+        <div class="header">
+            <div class="center">
+                <img src=""/>
+            </div>
+        </div>
+        
+       
+        <main class="conteudo">
+            <div class="center">
+                <div class="form">
+                    <h2> Fazer login no sistema </h2>
+                    <form method="POST"> 
+                        <div>    
+                            <label for="username">Username: </label>
+                            <input id="username" name="username" required>
+                        </div>
+        
+                        <div>    
+                            <label for="password">Password: </label>
+                            <input id="password" name="password" required type="password">    
+                        </div>    
+                    
+                        <p class="warning"> Campos obrigatórios </p>
+                        <input type="submit" value="Enviar">    
+                    </form>                
+                </div>
+        
+            </div>
+        </main>
+
+        <footer class="rodape">
+            <img class="img_rodape" src="imagens/rodape.png"></a>
+        </footer>
+        
+    </body>
+</html>
